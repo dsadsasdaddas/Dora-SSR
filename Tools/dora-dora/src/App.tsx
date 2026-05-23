29a9189c5d136779074e19f4c2ef8bf72a4df3be
wangyue <wangyue20060908@gmail.com>
2026-05-23 15:35:17 +0800

    feat(dora): add pixel editor tools

--- Tools/dora-dora/src/App.tsx
+++ Tools/dora-dora/src/App.tsx
@@ -53,6 +53,7 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import LLMConfigDialog from './LLMConfigDialog';
 import ProjectWorkspacePanel from './ProjectWorkspacePanel';
 import { createEmptyActionDocument, writeLegacyModel } from './ActionEditor';
+import { createEmptyPixelDocument, isPixelDocumentFile, writePixelDocument } from './PixelEditor/PixelDocument';
 
 const SpinePlayer = React.lazy(() => import('./SpinePlayer'));
 const Markdown = React.lazy(() => import('./Markdown'));
@@ -64,6 +65,7 @@ const TIC80Editor = React.lazy(() => import('./TIC80Editor'));
 const ActionEditor = React.lazy(() => import('./ActionEditor/ActionEditor'));
 const ActionClipPreview = React.lazy(() => import('./ActionEditor/ActionClipPreview'));
 const BodyEditor = React.lazy(() => import('./BodyEditor/BodyEditor'));
+const PixelEditor = React.lazy(() => import('./PixelEditor/PixelEditor'));
 
 const { path } = Info;
 
@@ -435,6 +437,9 @@ const getNewFileTemplate = (ext: string) => {
 			break;
 		case ".model":
 			content = writeLegacyModel(createEmptyActionDocument());
+			break;
+		case ".pixel.json":
+			content = writePixelDocument(createEmptyPixelDocument());
 			break;
 		case ".b.lua":
 			content = `return {"Array"}`;
@@ -1231,6 +1236,30 @@ export default function PersistentDrawerLeft() {
 				return;
 			}
 			const ext = path.extname(title).toLowerCase();
+			if (isPixelDocumentFile(title)) {
+				Service.read({ path: key }).then((res) => {
+					if (res.success && res.content !== undefined) {
+						const { content } = res;
+						const newFile: EditingFile = {
+							key,
+							title,
+							content,
+							contentModified: null,
+							folder: false,
+							status: "normal",
+							onMount: () => { },
+						};
+						newFile.onMount = onEditorDidMount(newFile);
+						resolve(newFile);
+					} else {
+						reject("file read error");
+					}
+				}).catch(() => {
+					addAlert(t("alert.read", { title }), "error");
+					reject("file read error");
+				});
+				return;
+			}
 			switch (ext) {
 				case ".png":
 				case ".jpg":
@@ -2631,6 +2660,7 @@ export default function PersistentDrawerLeft() {
 			case "Blockly": ext = ".bl"; break;
 			case "Wa": ext = ".wa"; break;
 			case "TIC80": ext = ".tic"; break;
+			case "Pixel Sprite": ext = ".pixel.json"; break;
 			case "Folder": ext = ""; break;
 			case "TypeScript": ext = ".tsx"; break;
 		}
@@ -4069,6 +4099,10 @@ export default function PersistentDrawerLeft() {
 						let actionModel = false;
 						let actionClip = false;
 						let bodyLua = false;
+						let pixelSprite = false;
+						if (isPixelDocumentFile(file.title)) {
+							pixelSprite = true;
+						}
 						switch (ext.toLowerCase()) {
 							case ".lua":
 								if (isBodyLuaFile(file.title) && !file.bodyTextEditing) {
@@ -4093,6 +4127,13 @@ export default function PersistentDrawerLeft() {
 							case ".vs": visualScript = true; break;
 							case ".tic": tic80 = true; break;
 							case ".model": actionModel = true; break;
+							case ".json":
+								if (pixelSprite) {
+									language = null;
+								} else {
+									language = "txt";
+								}
+								break;
 							case "": language = null; break;
 							default: language = "txt"; break
 						}
@@ -4170,6 +4211,23 @@ export default function PersistentDrawerLeft() {
 										</Suspense>
 									</MacScrollbar>
 								</div> : null
+							}
+							{pixelSprite ?
+								<Suspense fallback={<div />}>
+									<PixelEditor
+										filePath={file.key}
+										resourceBasePath={parentPath}
+										sourceContent={file.contentModified ?? file.content}
+										width={editorWidth}
+										height={editorHeight}
+										active={tabIndex === index}
+										readOnly={readOnly}
+										addAlert={addAlert}
+										onChange={(content) => {
+											setModified({ key: file.key, content });
+										}}
+									/>
+								</Suspense> : null
 							}
 							{yarn && !file.yarnTextEditing ?
 								<div style={{ display: 'flex', position: 'relative' }}>
